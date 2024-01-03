54d53953c97709e6e67ba6bfaa997bdf3c0a78ee
Kacper <komenda@student.agh.edu.pl>
2024-01-03 16:08:33 +0100

    e

---
+++ HandDetection/Dwiedlonie.py
@@ -0,0 +1,13 @@
+import mediapipe as mp
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon = 0.8, maxHands = 2)
+
+
+while True:
+    success, img = cap.read()
+    hands,img = detector.findHands(img, flipType= True)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
--- HandDetection/HandTrackingModule.py
+++ HandDetection/HandTrackingModule.py
@@ -4,7 +4,7 @@ import time
 import pyautogui
 
 
-class handDetector():
+class HandDetector():
     def __init__(self, mode=False, maxHands=2, modelComp=1, complexity = 1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
@@ -26,10 +26,11 @@ class handDetector():
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms,
-                                               self.mpHands.HAND_CONNECTIONS)
+                                               self.mpHands.HAND_CONNECTIONS,connection_drawing_spec=self.mpDraw.DrawingSpec(color=(0, 255, 0), thickness=2))
         return img
 
     def findPosition(self, img, handNo=0, draw=True):
+
         lmList = []
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -39,19 +40,9 @@ class handDetector():
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 # print(id, cx, cy)
                 lmList.append([id, cx, cy])
-                if len(lmList) >= 9:
-                    # Indeks kciuka (4) i wskazującego palca (8)
-                    thumb_x, thumb_y = lmList[4][1], lmList[4][2]
-                    index_x, index_y = lmList[8][1], lmList[8][2]
-                    cv2.line(img, (thumb_x, thumb_y), (index_x, index_y), (0, 255, 0), 5)
-                    cv2.circle(img, (thumb_x, thumb_y), 15, (255, 0, 255), cv2.FILLED)
-                    cv2.circle(img, (index_x, index_y), 15, (255, 0, 255), cv2.FILLED)
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-                    dist = ((thumb_x - index_x)**2 + (thumb_y - index_y)**2)**0.5
-                    if dist > 80:
-                        pyautogui.press("volumeup")
-                    else:
-                        pyautogui.press("volumedown")
         return lmList
 
 
@@ -59,10 +50,21 @@ def main():
     pTime = 0
     cTime = 0
     cap = cv2.VideoCapture(0)
-    detector = handDetector()
+    detector = HandDetector()
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
         lmList = detector.findPosition(img)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, "FPS: " + str(int(fps)), (10, 50), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+
+
+        cv2.imshow("Image", img)
+        if cv2.waitKey(1) == ord('q'): #kliknij q żeby wyjść
+            break
 if __name__ == "__main__":
     main()
